77052e438825945c94b48a22b8987a7479a497ba
kalanyr95 <osmankantar95@gmail.com>
2019-11-14 22:59:50 +0100

    model pokemon

---
+++ app/src/main/java/com/kalanyr/alphapokedex/PokemonList.java
@@ -0,0 +1,31 @@
+package com.kalanyr.alphapokedex;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PokemonList extends Fragment {
+
+
+    public PokemonList() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pokemon_list, container, false);
+    }
+
+}
